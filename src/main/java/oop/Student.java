a5edc69fee26d082983d25b6a668765ba5990c27
Пользователь <krutysh.super@yandex.ru>
2023-12-13 20:40:59 +0100

    1.2. Вызов метода объекта

---
+++ src/main/java/oop/Student.java
@@ -0,0 +1,17 @@
+package oop;
+
+public class Student {
+    public void sing() {
+        System.out.println("I believe I can fly");
+    }
+    public void music() {
+        System.out.println("Tra tra tra");
+}
+    public static void main(String[] args) {
+        Student petya = new Student();
+        int i = 0;
+        while (i++ < 3) {
+            petya.sing();
+        }
+    }
+}

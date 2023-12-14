af60b834064f04dddf0d57559b8657d45d6a4edf
Пользователь <krutysh.super@yandex.ru>
2023-12-14 19:53:06 +0100

    1.2

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -4,9 +4,11 @@ public class Student {
     public void sing() {
         System.out.println("I believe I can fly");
     }
+
     public void music() {
         System.out.println("Tra tra tra");
     }
+
     public static void main(String[] args) {
         int i = 0;
         Student petya = new Student();

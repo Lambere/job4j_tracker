e98aa3e72c3ccdff6de7c29018f257ed29e10044
Пользователь <krutysh.super@yandex.ru>
2023-12-16 09:20:49 +0100

    1.6

--- src/main/java/ru/job4j/oop/BallStory.java
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -4,8 +4,8 @@ public class BallStory {
     public static void main(String[] args) {
         Hare hare = new Hare();
         Fox fox = new Fox();
-        Wolf wolf = new Wolf();
         Ball kolobok = new Ball();
+        Wolf wolf = new Wolf();
         hare.tryEat(kolobok);
         wolf.tryEat(kolobok);
         fox.tryEat(kolobok);

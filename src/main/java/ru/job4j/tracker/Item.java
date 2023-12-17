4f7a68d5b39c19bb5fa44dce025f836e1ba5a2f4
Пользователь <krutysh.super@yandex.ru>
2023-12-17 13:41:05 +0100

    1.7.1

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,9 +1,19 @@
 package ru.job4j.tracker;
 
 public class Item {
-
     private int id;
     private String name;
+
+    public Item() {
+
+    }
+    public Item(String name) {
+        this.name = name;
+    }
+    public Item(int id, String name) {
+        this.name = name;
+        this.id = id;
+    }
 
     public int getId() {
         return id;

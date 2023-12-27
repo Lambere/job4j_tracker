74ef95f053074be5a39f61c91c472f38e7e748d1
Пользователь <krutysh.super@yandex.ru>
2023-12-27 10:37:48 +0100

    3

---
+++ src/main/java/ru/job4j/oop/Max.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+public class Max {
+    public static int max(int left, int right, int a) {
+
+        return max(max(left, right), a);
+    }
+
+    public static int max(int left, int right, int a, int b) {
+        int max = max(max(left, right), a);
+        return Math.max(b, max);
+    }
+
+
+    public static int max(int left, int right) {
+        return Math.max(left, right);
+    }
+}

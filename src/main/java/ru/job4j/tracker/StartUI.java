53fde2b4461d736cf0b28eb502bff648ecc4ee51
Пользователь <krutysh.super@yandex.ru>
2024-01-08 17:04:31 +0100

    6

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 
 public class StartUI {
+
     public static void main(String[] args) {
         Item item = new Item("dd-MMMM-EEEE-yyyy HH:mm:ss");
         LocalDateTime timeNow  = item.getCreated();

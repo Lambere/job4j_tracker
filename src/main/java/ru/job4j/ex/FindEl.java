180fa31c68a264bd391eecbfa996dd99058066e4
Lambere <krutysh.super@yandex.ru>
2024-02-17 23:35:30 +0100

    1

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,21 @@
+package ru.job4j.ex;
+
+public class FindEl {
+
+    public static int indexOf(String[] value, String key) {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if(value[i].equals(key)) {
+                result = i;
+            }
+        }
+        if ( result == -1) {
+            try {
+                throw new ElementNotFoundException();
+            } catch (ElementNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
+    }
+}

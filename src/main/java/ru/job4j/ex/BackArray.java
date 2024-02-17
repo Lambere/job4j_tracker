1a24820a3ffd67b790fd73ad70dfccde9a5eb262
Lambere <krutysh.super@yandex.ru>
2024-02-17 08:00:14 +0100

    1

--- src/main/java/ru/job4j/ex/BackArray.java
+++ src/main/java/ru/job4j/ex/BackArray.java
@@ -1,13 +1,16 @@
 package ru.job4j.ex;
 
-public class BackArray {
-    public String[] turn(String[] names) {
-        int middle = names.length / 2;
-        for (int index = 0; index < middle; index++) {
-            String temp = names[index];
-            names[index] = names[names.length - middle];
-            names[names.length - middle] = temp;
+    public class BackArray {
+
+        public String[] turn(String[] names) {
+            int middle = names.length / 2;
+            for (int index = 0; index < middle; index++) {
+                String temp = names[index];
+                names[index] = names[names.length - index - 1];
+                names[names.length - index - 1] = temp;
+            }
+            return names;
         }
-        return names;
     }
-}
+
+

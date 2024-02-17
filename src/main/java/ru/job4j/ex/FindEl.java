89300b6bec322cff2f5b73d114c448b445901dd6
Lambere <krutysh.super@yandex.ru>
2024-02-17 23:37:41 +0100

    1

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -5,11 +5,10 @@ public class FindEl {
     public static int indexOf(String[] value, String key) {
         int result = -1;
         for (int i = 0; i < value.length; i++) {
-            if(value[i].equals(key)) {
+            if (value[i].equals(key)) {
                 result = i;
             }
-        }
-        if ( result == -1) {
+        } if (result == -1) {
             try {
                 throw new ElementNotFoundException();
             } catch (ElementNotFoundException e) {

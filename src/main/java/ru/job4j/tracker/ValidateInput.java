c1c8516f56aeb5bed2aa01326b71195cd6c5569f
Lambere <krutysh.super@yandex.ru>
2024-02-23 07:55:35 +0100

    3

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -14,6 +14,7 @@ public class ValidateInput extends ConsoleInput {
                 System.out.println("Пожалуйста, введите корректные данные");
             }
         } while (invalid);
+
         return value;
     }
 }

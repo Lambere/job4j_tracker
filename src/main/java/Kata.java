3839e32029b3d9a7b039a91bf5dcc481bbf6e363
Lambere <krutysh.super@yandex.ru>
2024-02-25 08:28:52 +0100

    3

---
+++ src/main/java/Kata.java
@@ -0,0 +1,28 @@
+
+public class Kata {
+    public static int threeSumClosest(int[] nums, int target) {
+        int sum = 0;
+        int mostBad = Integer.MAX_VALUE;
+        int f = 0;
+        if (nums.length > 3) {
+            for (int i = 0; i < nums.length; i++) {
+                if (Math.abs(target) - Math.abs(nums[i]) > target - mostBad)  {
+                    mostBad = nums[i];
+                    f = i;
+                }
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i == f) {
+                continue;
+            }
+            sum += nums[i];
+        }
+        System.out.println(sum);
+        return sum;
+    }
+    public static void main(String[] args) {
+        threeSumClosest(new int[]{1,1,1,0}, 100);
+    }
+}

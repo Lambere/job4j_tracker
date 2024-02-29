47b2ac78b0e6f435519a927a4a58c6777eb568ba
Lambere <krutysh.super@yandex.ru>
2024-02-29 23:34:15 +0100

    SingleTracker

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        int numb  = name.equals("A380") ? 4 : COUNT_ENGINE;
+        int numb  = "A380".equals(name) ? 4 : COUNT_ENGINE;
         System.out.println("Количество двигателей равно: " + numb);
     }
 

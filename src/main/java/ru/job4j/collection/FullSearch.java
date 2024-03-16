96e8455aeb2bb8e62a75ce46d969173260bc57f3
Lambere <krutysh.super@yandex.ru>
2024-03-16 17:42:47 +0100

    FullSearch

--- src/main/java/ru/job4j/collection/FullSearch.java
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -5,10 +5,10 @@ import java.util.*;
 public class FullSearch {
 
     public Set<String> extractNumber(List<Task> tasks) {
-        Set<String> tasks1 = new HashSet<>();
+        Set<String> result = new HashSet<>();
         for (Task task : tasks) {
-            tasks1.add(task.getNumber());
+            result.add(task.getNumber());
         }
-        return tasks1;
+        return result;
     }
 }

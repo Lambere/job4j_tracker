cb9b9aa96eb8e3b69ae322794ca6ffa342ef126e
Lambere <krutysh.super@yandex.ru>
2024-03-17 20:11:49 +0100

    PriorityQueue

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -2,12 +2,25 @@ package ru.job4j.array;
 
 public class SimpleStringEncoder {
     public static String encode(String input) {
+        input = input + " ";
         String result = "";
-        char symbol = input.charAt(0);
-        int counter = 1;
-        /*
-        цикл fori
-         */
+        int a = 0;
+        char symbol = input.charAt(a);
+        int counter = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char symbol1 = input.charAt(i);
+            if (symbol == symbol1) {
+                counter++;
+            } else {
+                if (counter == 1 || counter == 0) {
+                    result = result + symbol;
+                } else {
+                    result = result + symbol + counter;
+                }
+                symbol = input.charAt(i);
+                counter = 1;
+            }
+        }
         return result;
     }
 }

8b9ad267db8483c2fe148bbc160c48379bc5769c
Lambere <krutysh.super@yandex.ru>
2024-03-18 18:45:42 +0100

    Student

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -10,10 +10,10 @@ public class Student {
     }
 
     public static void main(String[] args) {
-        int i = 0;
         Student petya = new Student();
-        while (i++ < 3) {
-            petya.sing();
-        }
+        petya.sing();
+        petya.sing();
+        petya.sing();
+
     }
 }

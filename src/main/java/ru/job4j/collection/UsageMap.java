d8070aa15cce1bd83658f39a516d3f2c6b9c8bcd
Lambere <krutysh.super@yandex.ru>
2024-03-20 06:39:42 +0100

    UsageMap

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> mail = new HashMap<>();
+        mail.put("andreypetrov@yandex.ru", "Andrey Petrov");
+        mail.put("andreylock@yandex.ru", "Andrey Lock");
+        mail.put("petyapupkin@yandex.ru", "Petya Pupkin");
+        for (String key : mail.keySet()) {
+            String value = mail.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}

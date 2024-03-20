10cc4b0c49291017367721687265c7a47260784a
Lambere <krutysh.super@yandex.ru>
2024-03-20 20:53:29 +0100

    TwoNumberSum

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -5,17 +5,13 @@ public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
         int j = array.length - 1;
         int i = 0;
-        while (i < j + 1) {
+        while (i < j) {
             if (array[i] + array[j] == target) {
                 return new int[]{i, j};
+            } else if (array[i] + array[j] < target) {
+                i++;
             } else {
-                if (i == array.length - 1) {
-                    j--;
-                    i *= 0;
-                } else {
-                   i++;
-                }
-
+                j--;
             }
         }
         return new int[0];
